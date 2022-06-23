73737ef3561ac6dc833698f3c5438da095c4844b
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2022-06-23 18:51:40 +0500

    fixed

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int var = 10;
-        int var1 = 1;
-        int var2 = 2;
-        int var3 = 3;
-        int var4 = 4;
-        int var5 = 5;
-        int var6 = 6;
-        int var7 = 7;
-        int var8 = 8;
+        int _variable = 10;
+        int variableOne = 1;
+        int variableTwo = 2;
+        int variableThree = 3;
+        int variableFour = 4;
+        int variableFive = 5;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
